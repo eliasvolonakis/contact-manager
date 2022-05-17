4bb07b96f77d4258ef21a5af2bab4575db59e1cb
Elias Volonakis <envolonakis@gmail.com>
2022-05-17 12:58:22 -0400

    Resolved uuid error

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { uuid } from 'uuidv4';
+import uuid from 'react-uuid';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
